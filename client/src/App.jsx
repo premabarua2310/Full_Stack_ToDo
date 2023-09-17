f39d438a326923df23f5c5d22a662ac619de1e51
premabarua2310 <premab1053@gmail.com>
2023-09-17 13:06:22 +0600

    Delete, Update, Edit option are added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,50 +1,71 @@
-/* eslint-disable no-unused-vars */
-import React, { useEffect, useState } from 'react';
-import ToDo from './components/ToDo';
+import  { useEffect, useState } from "react";
+import ToDo from "./components/ToDo";
 import axios from "axios";
-import { baseURL } from "./utils/Constant";
+import { baseURL } from "./utils/constant";
+import Popup from "./components/PopUp";
 
 const App = () => {
-
   const [toDos, setToDos] = useState([]);
-  const [input, setInput] = useState("")
+  const [input, setInput] = useState("");  
+  const [updateUI, setUpdateUI] = useState(false);
+  const [showPopup, setShowPopup] = useState(false);
+  const [popupContent, setPopupContent] = useState({});
 
   useEffect(() => {
     axios
       .get(`${baseURL}/get`)
       .then((res) => setToDos(res.data))
       .catch((err) => console.log(err));
-  }, []);
+  }, [updateUI]);
 
   const saveToDo = () => {
     axios
-      .post(`$(baseURL)/save`, { toDo: input } 
-        .then(res => {
-          console.log(res.data);
-        }))
-  }
+      .post(`${baseURL}/save`, { toDo: input })
+      .then((res) => {
+        console.log(res.data);
+        setUpdateUI((prevState) => !prevState);
+        setInput("");
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <main>
-      <div className='container'>
+      <div className="container">
         <h1 className="title">ToDo App</h1>
-        <div className='input_holder'>
+
+        <div className="input_holder">
           <input
             value={input}
             onChange={(e) => setInput(e.target.value)}
-            type='text'
-            placeholder='Add a ToDo...'
+            type="text"
+            placeholder="Add a ToDo..."
           />
-          <button>Add</button>
+          <button onClick={saveToDo}>Add</button>
         </div>
-        <div className='list'>
-          <ToDo />
-          <ToDo />
-          <ToDo />
+
+        <div className="list">
+          {toDos.map((el) => (
+            <ToDo
+              key={el._id}
+              text={el.toDo}
+              id={el._id}
+              setUpdateUI={setUpdateUI}
+              setShowPopup={setShowPopup}
+              setPopupContent={setPopupContent}
+            />
+          ))}
         </div>
       </div>
+      {showPopup && (
+        <Popup
+          setShowPopup={setShowPopup}
+          popupContent={popupContent}
+          setUpdateUI={setUpdateUI}
+        />
+      )}
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/components/ToDo.jsx
+++ client/src/components/ToDo.jsx
@@ -1,18 +1,32 @@
-/* eslint-disable no-unused-vars */
-import React from 'react';
+/* eslint-disable react/prop-types */
+import axios from "axios";
+// import React from "react";
 import { AiFillEdit } from "react-icons/ai";
 import { RxCross1 } from "react-icons/rx";
+import { baseURL } from "../utils/constant";
 
-const ToDo = () => {
-    return (
-        <div className='toDo'>
-            ToDo
-            <div className='icons'>
-                <AiFillEdit className='icon' />
-                <RxCross1 className='icon' />
-            </div>
-        </div>
-    )
-}
+const ToDo = ({ text, id, setUpdateUI, setShowPopup, setPopupContent }) => {
+  const deleteTodo = () => {
+    axios.delete(`${baseURL}/delete/${id}`).then((res) => {
+      console.log(res.data);
+      setUpdateUI((prevState) => !prevState);
+    });
+  };
 
-export default ToDo
+  const updateToDo = () => {
+    setPopupContent({ text, id });
+    setShowPopup(true);
+  };
+
+  return (
+    <div className="toDo">
+      {text}
+      <div className="icons">
+        <AiFillEdit className="icon" onClick={updateToDo} />
+        <RxCross1 className="icon" onClick={deleteTodo} />
+      </div>
+    </div>
+  );
+};
+
+export default ToDo;
